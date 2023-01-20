37fff02f11974aff285173fa15c48d292c9f01ed
Jean Honlet <jehon@users.noreply.github.com>
2023-01-20 10:35:53 +0100

    refactor: split ansible config

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,20 +3,9 @@
 set -o errexit
 set -o pipefail
 
-PRJ_ROOT="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")"
+SWD="$(dirname "${BASH_SOURCE[0]}")"
+
+PRJ_ROOT="$(dirname "$SWD")"
 export PRJ_ROOT
 
-apt update
-
-apt_install() {
-    # /repo is not always available
-    DEBIAN_FRONTEND=noninteractive apt install --quiet --yes "$@"
-}
-
-echo "*** Configure python3"
-apt_install python-is-python3 python3 python3-pip python3-netaddr
-
-if [ ! -r /etc/jehon/restricted/ansible-key ]; then
-    mkdir -p /etc/jehon/restricted
-    echo "1234" > /etc/jehon/restricted/ansible-key
-fi
+$SWD/setup-ansible.sh
