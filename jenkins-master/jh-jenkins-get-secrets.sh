ca988cd642938a087e64d03cc675cfb332a1ef99
Jean Honlet <jehon@users.noreply.github.com>
2023-01-23 10:08:15 +0100

    feat: factorize ansible lib

--- jenkins-master/jh-jenkins-get-secrets.sh
+++ jenkins-master/jh-jenkins-get-secrets.sh
@@ -3,7 +3,11 @@
 set -o errexit
 
 . /usr/bin/jh-lib
+DS_ROOT="$( dirname "$JH_SWD" )"
+
 . "$JH_SWD"/jenkins-bin/jenkins-lib.sh
+. "$DS_ROOT/ansible/lib.sh"
+
 
 #
 # About ? 
@@ -12,14 +16,21 @@ set -o errexit
 #
 #  Thanks to https://stackoverflow.com/a/38474024/1954789
 #
-TARGET="$JH_SWD/tmp"
+TARGET="$DS_ROOT/tmp/jenkins"
 mkdir -p "$TARGET"
-
 
 echo "## Backup secrets..."
 docker compose cp "$JENKINS_DOCKER_NAME:$JENKINS_GUEST_HOME/secrets/hudson.util.Secret" "$TARGET/"
 docker compose cp "$JENKINS_DOCKER_NAME:$JENKINS_GUEST_HOME/secrets/master.key" "$TARGET/"
 echo "## Backup secrets done"
+
+echo "# Encrypting secrets..."
+cd "$DS_ROOT/ansible"
+echo "## Encrypting master.key"
+cat $TARGET/master.key | ansible-vault encrypt_string > $TARGET/master.key.encrypted
+echo "## Encrypting hudson.util.Secret"
+cat $TARGET/hudson.util.Secret | base64 | ansible-vault encrypt_string > $TARGET/hudson.util.Secret.encrypted
+echo "# Encrypting secrets done"
 
 echo "***"
 echo "* Secrets are at $TARGET"
