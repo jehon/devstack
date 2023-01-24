051dad8468ef7dd200f05980345ff1f244c4b04c
Jean Honlet <jehon@users.noreply.github.com>
2023-01-24 08:33:32 +0100

    fix: token handling

--- ansible/roles/host_dev/templates/jenkins/env.sh
+++ ansible/roles/host_dev/templates/jenkins/env.sh
@@ -3,4 +3,4 @@
 set -o errexit
 
 export JENKINS_SYSTEM_USER="{{ jehon.credentials.jenkins.system_user }}"
-export JENKINS_SYSTEM_KEY="{{ jehon.credentials.jenkins.system_key }}"
+export JENKINS_SYSTEM_KEY="{{ jehon.credentials.jenkins.system_token }}"
