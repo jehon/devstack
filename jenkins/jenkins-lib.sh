6156db102e0b10f4f11603a9f89d2ebf771b6e63
Jean Honlet <jehon@users.noreply.github.com>
2023-01-24 18:14:27 +0100

    fix(jenkins): using treafik

--- jenkins/jenkins-lib.sh
+++ jenkins/jenkins-lib.sh
@@ -8,7 +8,7 @@ export TMP="$SWD/../tmp/jenkins"
 export JENKINS_JAR="$TMP/jenkins-cli.jar"
 export JENKINS_DOCKER_NAME="jenkins"
 export JENKINS_GUEST_HOME="/var/jenkins_home"
-export JENKINS_URL="http://localhost:8080/jenkins"
+export JENKINS_URL="http://localhost/jenkins/"
 export DS_ROOT="$SWD/../"
 
 if [ -r /etc/jehon/restricted/jenkins.env ]; then
