1c38c298d7e3db605b7e2a7a4d34254ba99f2fe9
Jean Honlet <jehon@users.noreply.github.com>
2023-01-25 16:27:31 +0100

    docs: sysctl in comments

--- start.sh
+++ start.sh
@@ -6,6 +6,20 @@ set -o pipefail
 ROOT="$(realpath "$( dirname "${BASH_SOURCE[0]}")")"
 cd "$ROOT"
 
+# if [ $(sysctl --value vm.max_map_count) -lt 262144 ]; then
+#     echo "Adapting max_map_count"
+#     # 65530 => 262144
+#     sudo sysctl -w vm.max_map_count=262144
+# fi
+
+#
+# Sonar need (elasticSearch in fact):
+# sysctl -w vm.max_map_count=524.288 <= 65.530
+# sysctl -w fs.file-max=131072
+# ulimit -n 131072
+# ulimit -u 8192
+#
+
 mkdir -p jenkins/built/secrets
 if [ ! -r jenkins/built/secrets/master.key ]; then
     cp -f /etc/jehon/restricted/jenkins-master.key jenkins/built/secrets/master.key
