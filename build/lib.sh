3033074a96a8c602c759f1120c1004a585cd1fed
Jean Honlet <jehon@users.noreply.github.com>
2023-01-25 16:49:39 +0100

    fix: autocreate lib

--- build/lib.sh
+++ build/lib.sh
@@ -7,6 +7,9 @@ PRJ_ROOT="$( dirname "$(realpath "$( dirname "${BASH_SOURCE[0]}")")" )"
 PRJ_TMP="$PRJ_ROOT/tmp"
 
 export PRJ_ROOT
+export PRJ_TMP
+
+mkdir -p "$PRJ_TMP"
 
 runDockerCompose() {
     docker compose --env-file=/etc/jehon/restricted/jenkins.env "$@"
