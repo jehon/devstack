25556212d41a295d9928187e787c5c19e9bb5a4b
Jean Honlet <jehon@users.noreply.github.com>
2023-01-25 22:28:03 +0100

    fix: generate built data for ansible

--- .devcontainer/ansible-requirements.sh
+++ .devcontainer/ansible-requirements.sh
@@ -15,8 +15,3 @@ apt_install() {
 
 echo "*** Configure python3"
 apt_install python-is-python3 python3 python3-pip python3-netaddr
-
-if [ ! -r "$ROOT"/ansible/built/ansible-key ]; then
-    mkdir -p "$ROOT"/ansible/built
-    echo "1234" > "$ROOT"/ansible/built/ansible-key
-fi
--- build/ansible-install.sh
+++ build/ansible-install.sh
@@ -3,9 +3,39 @@
 set -o errexit
 set -o pipefail
 
-pip install --upgrade --target .python ansible ansible-lint ansible-vault
-
 PWD="$(pwd)"
 
-export PATH="$PWD/.python/bin:$PATH"
-export PYTHONPATH="$PWD/.python:$PATH"
+SWD="$(realpath "$( dirname "${BASH_SOURCE[0]}")")"
+. "$SWD"/lib.sh
+
+# pip install --upgrade --target .python ansible ansible-lint ansible-vault
+
+# export PATH="$PWD/.python/bin:$PATH"
+# export PYTHONPATH="$PWD/.python:$PATH"
+
+mkdir -p "$PRJ_ROOT"/ansible/built
+
+if [ ! -r "$PRJ_ROOT"/ansible/built/ansible-key ]; then
+    if [ -r /etc/jehon/restricted/ansible-key ]; then
+        echo "* Linking ansible-key to restricted one"
+        ln -fs /etc/jehon/restricted/ansible-key "$PRJ_ROOT"/ansible/built/ansible-key
+    else
+        echo "* Creating dummy ansible-key"
+        echo "1234" > "$PRJ_ROOT"/ansible/built/ansible-key
+    fi
+fi
+
+if [ ! -r "$PRJ_ROOT"/ansible/built/ansible_ssh_key ]; then
+    if [ -r /etc/jehon/restricted/ansible_ssh_key ]; then
+        echo "* Linking ansible_ssh_key to restricted one"
+        ln -fs /etc/jehon/restricted/ansible_ssh_key "$PRJ_ROOT"/ansible/built/ansible_ssh_key
+    else 
+        if [ -r $HOME/.ssh/id_rsa ]; then
+            echo "* Linking ansible_ssh_key to home ssh key"
+            ln -fs $HOME/.ssh/id_rsa "$PRJ_ROOT"/ansible/built/ansible_ssh_key
+        fi
+    fi
+fi
+
+echo "* Allow direnv"
+direnv allow "$PRJ_ROOT"/ansible
