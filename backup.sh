d924174443e9de6d4d33396ca31b759f65600004
Jean Honlet <jehon@users.noreply.github.com>
2023-02-02 10:36:00 +0100

    fix: rename scripts

--- backup.sh
+++ backup.sh
@@ -2,12 +2,13 @@
 
 set -o errexit
 set -o pipefail
+shopt -s nullglob
 
 SWD="$(realpath "$( dirname "${BASH_SOURCE[0]}")")"
 
-for S in */backup.sh ; do
-    echo "*** backup in $( dirname "$S") "
-    "$S"
+for S in */*-backup.sh ; do
+    echo "*** backup $( basename "$S") "
+    # "$S"
 done
 
 echo "*** All done"
