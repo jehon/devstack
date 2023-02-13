fe989f07ef3bd2b4eca8870ddc07d71afa586fc1
Jehon <1582670+jehon@users.noreply.github.com>
2023-02-13 18:31:56 +0100

    Add jehon.deb

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,4 +8,8 @@ SWD="$(dirname "${BASH_SOURCE[0]}")"
 PRJ_ROOT="$(dirname "$SWD")"
 export PRJ_ROOT
 
+mkdir tmp
+curl -fsSL https://jehon.github.io/packages/jehon.deb -o tmp/jehon.deb
+apt install --yes ./tmp/jehon.deb
+
 $SWD/ansible-requirements.sh
