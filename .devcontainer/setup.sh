09c14a3f02a4b01e8423d579f7fe596571261cb0
Jean Honlet <jehon@users.noreply.github.com>
2023-03-13 08:47:57 +0100

    ci: start by apt update

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,9 +8,11 @@ SWD="$(dirname "${BASH_SOURCE[0]}")"
 PRJ_ROOT="$(dirname "$SWD")"
 export PRJ_ROOT
 
+apt update
+
 mkdir tmp
 curl -fsSL https://jehon.github.io/packages/jehon.deb -o tmp/jehon.deb
 chmod 777 tmp
 apt install --yes ./tmp/jehon.deb
 
-$SWD/ansible-requirements.sh
+"$SWD"/ansible-requirements.sh
